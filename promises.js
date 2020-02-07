d17189f0281ababc5cb9ddc39e5350be2d020cd0
George <admin@georgesimos.com>
2020-02-07 14:59:04 +0200

    Async / Await

--- promises.js
+++ promises.js
@@ -33,15 +33,27 @@ function createPost(post) {
 //   .then(getPosts)
 //   .catch(console.error);
 
+// Async / Await
+async function init() {
+  await createPost({
+    title: "Post three",
+    body: "this is post three!"
+  });
+
+  getPosts();
+}
+
+init();
+
 // Promise.all
-const promise1 = Promise.resolve("Hello World");
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(resolve, 2000, "Goodbye")
-);
-const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
-  res.json()
-);
-Promise.all([promise1, promise2, promise3, promise4]).then(values =>
-  console.log(values)
-);
+// const promise1 = Promise.resolve("Hello World");
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//   setTimeout(resolve, 2000, "Goodbye")
+// );
+// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
+//   res.json()
+// );
+// Promise.all([promise1, promise2, promise3, promise4]).then(values =>
+//   console.log(values)
+// );
